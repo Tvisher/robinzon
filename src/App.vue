2c7753ba3ef6fd48ad6fafec71a48fe3646cb73e
Ivan Tvisher <Tvisher@mail.ru>
2024-04-15 16:25:55 +0500

    start

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <body class="body">
+    <div class="home-page">
+      <header class="header"></header>
+      <main class="main">
+        <div class="home-page__wrapper">
+          <div class="home-page__content">
+            <div class="left-blur">
+              <img src="./assets/img/left-blur.svg" alt="" />
+            </div>
+            <div class="right-blur">
+              <img src="./assets/img/right-blur.svg" alt="" />
+            </div>
+
+            <div class="home-page__content-inner">
+              <h1 class="home-page__title">
+                <span class="color-title">{ Привет }</span> <br />
+                Меня зовут Робинзон!
+              </h1>
+              <span class="home-page__descr">
+                Я найду для вас самую актуальную информацию по любой теме в
+                открытых источниках и подготовлю в формате структурированного
+                отчета, который удобно читать и понимать.
+              </span>
+
+              <form class="home-page__form" @submit.prevent="getRequest">
+                <label class="form-label">
+                  <input
+                    type="text"
+                    class="form-field"
+                    placeholder="Что будем исследовать?"
+                    v-model.trim="requestMessage"
+                  />
+                  <button type="submit" class="form-btn">ИССЛЕДОВАТЬ</button>
+                </label>
+              </form>
+            </div>
+          </div>
+        </div>
+      </main>
+      <footer class="footer"></footer>
+    </div>
+  </body>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const requestMessage = ref("");
+const getRequest = () => {
+  console.log(requestMessage.value);
+};
+</script>
+<style scoped></style>
---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  base: './',
+  // base: '/analytics-service/',
+  build: {
+    outDir: './docs',
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
+})
+
